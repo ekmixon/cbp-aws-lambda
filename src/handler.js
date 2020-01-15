63a05b7bb3e96c5fed2492d12431e2f88dd53244
Kevin Flaherty <kevin.flaherty@casebook.net>
2020-01-15 11:47:34 -0500

    initial handler and testHandler

---
+++ src/handler.js
@@ -0,0 +1,12 @@
+exports.handler = (event, context, callback) => {
+    if(event.triggerSource === "CustomMessage_AdminCreateUser" || event.triggerSource === "CustomMessage_ResendCode") {
+        event.response.emailSubject = "Welcome to the Casebook Platform";
+        event.response.emailMessage = "Thank you for signing up. Your username is " + event.request.usernameParameter + " and " + event.request.codeParameter + " is your temporary password.";
+    }
+    else if(event.triggerSource === "CustomMessage_ForgotPassword") {
+        event.response.emailSubject = "Forgot Password for Casebook Platform";
+        event.response.emailMessage = "Your username is " + event.request.usernameParameter + " and " + event.request.codeParameter + " is your verification code \n."
+        + "Reset your password at internal.casebookdev.net/authentication/reset-password";
+    }
+    callback(null, event);
+};
